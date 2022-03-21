26777b7ebef015c8cb80e9276d96591fde557f28
datatags <datatags@users.noreply.github.com>
2022-03-20 19:17:17 -0700

    Initial commit

---
+++ src/me/datatags/racingstructures/RacingStructures.java
@@ -0,0 +1,10 @@
+package me.datatags.racingstructures;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RacingStructures extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        
+    }
+}
